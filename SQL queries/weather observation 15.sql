030005c4adbaa3f416502b841201501313d8f1b9
cesarcamp <danielsuarez8509@gmail.com>
2020-09-08 18:57:06 -0500

    weeather observation 15

---
+++ SQL queries/weather observation 15.sql
@@ -0,0 +1,10 @@
+-- Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N)
+-- in STATION that is less than . Round your answer to  decimal places.
+
+select round(long_w,4)
+from station
+where lat_n = (select lat_n
+    from station
+    where lat_n < 137.2345
+    order by lat_n desc
+    limit 1);
